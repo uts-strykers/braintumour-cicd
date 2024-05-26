f96e78cccb0c4f5a3cdeeb38affe84d51b7ba1ca
Sotero Jr Saberon <sotero.saberon.jr@gmail.com>
2024-05-26 10:21:20 +1000

    fix bugs

--- launch_training.task.py
+++ launch_training.task.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     task = Task.init(
-        project_name="My ML Project", task_name="Model Training on Latest Script"
+        project_name="BrainScan2", task_name="TrainModel"
     )
     # Print the task ID in a format that can be easily captured
     print(f"TASK_ID_OUTPUT: {task.id}")
     task.set_repo(repo=args.repo_url, branch=args.branch_name, commit=args.commit_hash)
-    task.execute_remotely(queue_name="gitarth", exit_process=True)
+    task.execute_remotely(queue_name="uts-strykers-queue", exit_process=True)
     train_and_evaluate(task)
